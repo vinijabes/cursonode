acd8f6fb9294aba8f297036b79670f92f0cc0b07
Renan Pupin <renan.pupin@gmail.com>
2018-09-25 23:58:22 -0300

    change port heorku

--- server.js
+++ server.js
@@ -90,8 +90,9 @@ app.delete('/todo/:id', async(req, res) => {
 });
 
 //SERVER listening
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
+let port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('Example app listening on port '+port);
 });
 
 //add exports to app at server.js
